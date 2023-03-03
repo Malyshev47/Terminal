86686ebdb49fca14ed08556223b85041c62ccf7c
Malyshev47 <slk332@yandex.ru>
2023-03-03 23:41:05 +0300

    add 3 new file to txt-reposit

---
+++ firstscript.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Hello!
+
+mkdir cc1 cc2 cc3
+cd cc1
+touch file1.txt file2.txt file3.txt js1.json js2.json
+mkdir vv1 vv2 vv3
+ls -la
+mv {file1.txt,file2.txt} "C:\Users\slk33\Desktop\GIT\VK\hw1\script\cc2"
